257153f7b0a66d0f814927e728a72148de71647f
sowaibathecoder <official.sowaibanaz@gmail.com>
2026-01-15 00:49:15 +0500

    minor changes

--- backend/src/routes/__init__.py
+++ backend/src/routes/__init__.py
@@ -5,4 +5,4 @@ from fastapi import APIRouter
 from . import tasks
 
 router = APIRouter()
-router.include_router(tasks.router, prefix="/tasks", tags=["tasks"])
+router.include_router(tasks.router, tags=["tasks"])
